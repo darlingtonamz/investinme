81f74e942437b8dcc8152069ca8be27dfc40df12
Edwin Tsatsu <paynegreen@yahoo.com>
2016-03-18 13:57:21 +0000

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :categories
   resources :users do
-    resources :campaigns do
+    member do
+      resources :campaigns do
+    end
       resources :payments
-    end 
+    end
   end
 end
