a6787f373512b13e20319200029045f735199859
Edwin Tsatsu <paynegreen@yahoo.com>
2016-03-18 13:44:05 +0000

    fixed model relationships

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,5 @@
 class Campaign < ActiveRecord::Base
-	belongs_to user
-	has_many payments
-	has_and_belongs_to_many categories
+	belongs_to :user
+	has_many :payments
+	has_and_belongs_to_many :categories
 end
