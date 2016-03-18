4b5d980db74f51a9508127a61ce5dd850dcca9e4
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-03-18 17:32:23 +0000

    Add payment integration from vodafone

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
 
   resources :categories
   resources :users do
-    member do
       resources :campaigns do
         resources :payments
       end
-    end
   end
 end
